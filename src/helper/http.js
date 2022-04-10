d9c10a5cbe9be6b1e23845d761381b243e315ea5
asherginting <asherginting3@gmail.com>
2022-04-11 00:02:20 +0700

    fix: update helper & ip

--- src/helper/http.js
+++ src/helper/http.js
@@ -1,14 +1,12 @@
 import axios from 'axios';
 
-const BACKEND_URL = '192.168.32.195:5000';
-
 const http = token => {
   const headers = {};
   if (token) {
     headers.Authorization = `Bearer ${token}`;
   }
   return axios.create({
-    baseURL: 'http://192.168.32.195:5000',
+    baseURL: 'http://192.168.247.222:5000',
     headers,
   });
 };
